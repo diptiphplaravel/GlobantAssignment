fa756c7595fab25ed20c2001506d95717341018c
diptichandegave <41861936+diptichandegave@users.noreply.github.com>
2018-08-25 22:50:50 +0530

    Update User.php

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
+
 class User extends Authenticatable
 {
     use Notifiable;
